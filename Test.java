13708cf2a7b448f849cdad66b1e5b4ae9318156b
prashant-subhash <prashant.kumar2@wissen.com>
2025-04-07 14:25:56 +0530

    added logs to Test

--- Test.java
+++ Test.java
@@ -2,5 +2,9 @@ class Test
 {
 	public Static void main(String[] args)
 	{
+		System.out.println("hello git world");
+		int a = 1, b = 2;
+		int sum = a+b;
+		System.out.printf("The sum is : %d%n",sum);
 	}
 }
